930086705de5ee94e768bc0e1521799d5d67da22
Tom Galluzzo <tgalluzzo@rec.ri.cmu.edu>
2010-03-10 08:30:05 -0500

    Fixes for older versions of QNX

--- libopenJaus/include/utils/hashMap.h
+++ libopenJaus/include/utils/hashMap.h
@@ -49,7 +49,10 @@
 	#define HASH_MAP stdext::hash_map
 #elif defined(__GNUC__)
 	#if defined(__QNX__)
-		#include <hash_map>                             // QNX 6.3 - GNU 3.3.5
+		#include <hash_map>
+		#ifndef _NAMESPACE_STLPORT
+			#define _NAMESPACE_STLPORT std
+		#endif
 		#define HASH_MAP _NAMESPACE_STLPORT::hash_map   // QNX 6.3 - GNU 3.3.5
 	#else
 		#include <features.h>
