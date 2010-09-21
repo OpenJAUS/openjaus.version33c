56621162d3e4b2bb2557b083579a1d76bd65eb72
robm <rmeyers@mail.rec.ri.cmu.edu>
2010-09-21 19:26:39 -0400

    - Condensed the premake4 scripts into one main script

--- premake4.lua
+++ premake4.lua
@@ -1,9 +1,105 @@
 solution "OpenJAUS"
 	configurations { "Debug", "Release" }
 	
-	include "libjaus"
-	include "libopenJaus"
-	include "ojNodeManager"
-	include "ojVehicleSim"
+project "jaus"
+  local prj = project()
+  local sol = solution()
+  kind "StaticLib"
+  language "C"
+
+  prj.basedir = string.format("%s/lib%s", sol.basedir, prj.name)
+  os.chdir(prj.basedir)
+  printf("Project \"%s\" base directory: %s", prj.name, prj.basedir)
+
+  targetdir "lib"
+  objdir ".build"
+  includedirs { "include"}
+
+  files { "**.h", "**.c", "**.cpp" }
+
+  configuration "Debug"
+    defines { "DEBUG" }
+    flags { "Symbols" }
+      
+  configuration "Release"
+    defines { "NDEBUG" }
+    flags { "Optimize" }
+
+
+project "openJaus"
+  local prj = project()
+  local sol = solution()
+  kind "StaticLib"
+  language "C"
+
+  prj.basedir = string.format("%s/lib%s", sol.basedir, prj.name)
+  os.chdir(prj.basedir)
+  printf("Project \"%s\" base directory: %s", prj.name, prj.basedir)
+
+  targetdir "lib"
+  objdir ".build"
+  includedirs { "include", "../libjaus/include" }
+
+  files { "**.h", "**.c", "**.cpp" }
+    
+  configuration "Debug"
+    defines { "DEBUG" }
+    flags { "Symbols" }
+      
+  configuration "Release"
+    defines { "NDEBUG" }
+    flags { "Optimize" }
+
+
+project "ojNodeManager"
+  local prj = project()
+  local sol = solution()
+  kind "ConsoleApp"
+  language "C++"
+
+  prj.basedir = string.format("%s/%s", sol.basedir, prj.name)
+  os.chdir(prj.basedir)
+  printf("Project \"%s\" base directory: %s", prj.name, prj.basedir)
+
+  targetdir "bin"  
+  objdir ".build"
+  includedirs { "include", "../libjaus/include", "../libopenJaus/include"}
+
+  files { "**.cpp" }
+  links { "openJaus", "jaus", "pthread" }
+
+  configuration "Debug"
+    defines { "DEBUG" }
+    flags { "Symbols" }
+      
+  configuration "Release"
+    defines { "NDEBUG" }
+    flags { "Optimize" }
+    
+    
+project "ojVehicleSim"
+  local prj = project()
+  local sol = solution()
+  kind "ConsoleApp"
+  language "C++"
+
+  prj.basedir = string.format("%s/%s", sol.basedir, prj.name)
+  os.chdir(prj.basedir)
+  printf("Project \"%s\" base directory: %s", prj.name, prj.basedir)
+
+  targetdir "bin"  
+  objdir ".build"
+  includedirs { "include", "../libjaus/include", "../libopenJaus/include"}
+  
+  files { "**.h", "**.c", "**.cpp" }
+  links { "openJaus", "jaus", "pthread", "ncurses" }
+
+  configuration "Debug"
+    defines { "DEBUG" }
+    flags { "Symbols" }
+
+  configuration "Release"
+    defines { "NDEBUG" }
+    flags { "Optimize" }
 	
 
